ef5bb4836661ac0ad596bd5b2637575d63f55f9f
rain567 <1585918524@qq.com>
2022-03-05 22:12:30 +0800

    修改

--- src/views/common/home.vue
+++ src/views/common/home.vue
@@ -2,26 +2,7 @@
   <div class="mod-home">
     <h3>项目介绍</h3>
     <ul>
-      <li>renren-fast-vue基于vue、element-ui构建开发，实现<a href="https://gitee.com/renrenio/renren-fast" target="_blank">renren-fast</a>后台管理前端功能，提供一套更优的前端解决方案</li>
-      <li>前后端分离，通过token进行数据交互，可独立部署</li>
-      <li>主题定制，通过scss变量统一一站式定制</li>
-      <li>动态菜单，通过菜单管理统一管理访问路由</li>
-      <li>数据切换，通过mock配置对接口数据／mock模拟数据进行切换</li>
-      <li>发布时，可动态配置CDN静态资源／切换新旧版本</li>
-      <li>演示地址：<a href="http://demo.open.renren.io/renren-fast" target="_blank">http://demo.open.renren.io/renren-fast</a> (账号密码：admin/admin)</li>
-    </ul>
-    <h3>获取帮助</h3>
-    <ul>
-      <li>官方社区：<a href="https://www.renren.io/community" target="_blank">https://www.renren.io/community</a></li>
-      <li>前端Git地址：<a href="https://github.com/renrenio/renren-fast-vue" target="_blank">https://github.com/renrenio/renren-fast-vue</a></li>
-      <li>后台Git地址：<a href="https://gitee.com/renrenio/renren-fast" target="_blank">https://gitee.com/renrenio/renren-fast</a></li>
-      <li>代码生成器：<a href="https://gitee.com/renrenio/renren-generator" target="_blank">https://gitee.com/renrenio/renren-generator</a></li>
-      <li>如需关注项目最新动态，请Watch、Star项目，同时也是对项目最好的支持</li>
-    </ul>
-    <h3>官方QQ群</h3>
-    <ul>
-      <li>高级群：324780204(大牛云集，跟大牛学习新技能)</li>
-      <li>普通群：145799952(学习交流，互相解答各种疑问)</li>
+      本系统采用的开发工具是IDEA和Visual Studio Code。 是一个开放源代码的、基于Java的可扩展开发平台。就其本身而言，它只是一个框架和一组服务，用于通过插件组件构建开发环境，但是 附带了一个标准的插件集，包括Java开发工具（Java Development Kit、JDK）。有免费、纯java语言编写、免安装、扩展性强等特点，可以让界面的图形更加美观，缓解了Java枯燥的开发图形界面的过程。使用中文版的开发工具，编程时更加便于理解，在书写代码的过程中会有代码提示，也提高了设计效率。非常适合本系统的设计与实现。前端网页的设计采用Visual Studio Code工具进行开发，Visual Studio Code有着免费、开源、轻量、跨平台的特点。
     </ul>
   </div>
 </template>
